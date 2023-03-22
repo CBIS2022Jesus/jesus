48eef1c99646832b981bcab018fdd95baf7f99a3
CBIS2022Jesus <123964362+CBIS2022Jesus@users.noreply.github.com>
2023-03-22 11:57:13 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+let happy = images.createImage(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+let sad = images.createImage(`
+    . . . . .
+    . # . # .
+    . . . . .
+    . # # # .
+    # . . . #
+    `)
+let scared = images.createImage(`
+    . # . # .
+    . . . . .
+    . # # # .
+    # . . . #
+    . # # # .
+    `)
+let exsited = images.createBigImage(`
+    . . # # . . # # . .
+    . . # # . . # # . .
+    . # . . . . . . # .
+    . # . . . . . . # .
+    . . # # # # # # . .
+    `)
+basic.forever(function () {
+	
+})
